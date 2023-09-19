5db0e377f8afb20efc6575d4cfca6b53ca5ba50b
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2023-09-19 09:57:12 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -1,28 +1,29 @@
 import os
 from environs import Env
+
+
 env = Env()
 env.read_env()
 
 
 DATABASES = {
     'default': {
-        'ENGINE': env("ENGINE"),
-        'HOST': env("HOST"),
-        'PORT': env("PORT"),
-        'NAME': env("NAME"),
-        'USER': env("USER"),
-        'PASSWORD': env("PASSWORD"),
+        'ENGINE': env("DB_ENGINE"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
     }
 }
 
-INSTALLED_APPS = [env("INSTALLED_APPS")]
+INSTALLED_APPS = ['datacenter']
 
 SECRET_KEY = env("SECRET_KEY")
 
-
 DEBUG = env.bool("DEBUG_FALSE", "DEBUG_TRUE")
 
-ROOT_URLCONF = env("ROOT_URLCONF")
+ROOT_URLCONF = 'project.urls'
 
 ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
 
